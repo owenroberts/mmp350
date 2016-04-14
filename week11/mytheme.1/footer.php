894eb728d32cc46577eb0b0ea6d91400a52d1226
Owen Roberts <obr208@nyu.edu>
2016-04-13 20:12:53 -0400

    adds foundation .scss styles to wp theme

---
+++ week11/mytheme.1/footer.php
@@ -0,0 +1,8 @@
+		<footer>
+			<p class="copyright">&copy; <?php echo the_date("Y"); ?></p>
+			<p class="webdesigner">website by <?php the_author(); ?></p>
+		</footer>
+
+	</div> <!-- end wrapper -->
+</body>
+</html>
---
+++ week11/mytheme.1/functions.php
@@ -0,0 +1,4 @@
+<?php 
+    register_nav_menu( 'main-menu', 'Main Menu');
+    add_theme_support( 'post-thumbnails' );
+?>
