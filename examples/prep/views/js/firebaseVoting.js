82a60a2715c7bd535a27f374fd21e7e066964385
owenroberts <obr208@nyu.edu>
2018-01-25 13:59:03 -0500

    done w prep ex

--- examples/prep/views/js/firebaseVoting.js
+++ examples/prep/views/js/firebaseVoting.js
@@ -1,6 +1,53 @@
 function addRest() {
 	const db = firebase.database();
 	const restRef = db.ref('/rest');
-	const name = document.getElementById('addRest').value;
-	return restRef.push(name);
+	const restInput = document.getElementById('addRest')
+	const name = restInput.value;
+	restInput.value = "";
+	restRef.push({
+			name: name,
+			upvoters: {},
+			downvotes: {}
+		})
+		.then(function() {
+			window.location.reload();
+		})
+		.catch(function(err){
+			console.log('error', err);
+		});
+}
+
+function upvote(key) {
+	const db = firebase.database();
+	const user = firebase.auth().currentUser;
+	const userId = user.uid;
+	const userName = user.displayName;
+	const restRef = db.ref('/rest')
+		.child(key)
+		.child('/votes')
+		.child(userId);
+	restRef.push(userName)
+		.then(function(){
+			window.location.reload();
+		})
+		.catch(function(err){
+			console.log('err', error);
+		});
+}
+
+function downvote(key) {
+	const db = firebase.database();
+	const user = firebase.auth().currentUser;
+	const userId = user.uid;
+	const restRef = db.ref('/rest')
+		.child(key)
+		.child('/votes')
+		.child(userId)
+		.remove()
+		.then(function(){
+			window.location.reload();
+		})
+		.catch(function(err){
+			console.log('err', error);
+		});
 }
