877621f5db3a1293c8d1a2b6aee601507dc6e837
Owen Roberts <oroberts@bmcc.cuny.edu>
2018-02-28 21:20:43 -0500

    new node app week 4

--- examples/new_node_app/server.js
+++ examples/new_node_app/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(logger('dev'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.get('/', function(request, response) {
-	response.send('<h1>Welcome to my website.</h1>');
+	response.render('home.ejs');
+});
+
+app.post('/', function(request, response) {
+	const name = request.body.username;
+	response.render('login.ejs', {
+		username: name
+	});
 });
 
 const port = process.env.PORT || 8000;
 app.listen(port, function() {
 	console.log("App running on port", port);
 });
+
+
+
