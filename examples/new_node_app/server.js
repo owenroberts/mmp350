8faf73a6f0230b3f9525b1b0966b7d5b164bfdd8
Owen Roberts <oroberts@bmcc.cuny.edu>
2018-03-19 18:54:48 -0400

    week 6 node ex

--- examples/new_node_app/server.js
+++ examples/new_node_app/server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+const serviceAccount = require('./key.json');
+
+const firebaseAdmin = admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://new-node-app-b198a.firebaseio.com'
+});
+
+function isAuthenticated(request, response, next) {
+	const uid = request.query.uid;
+	firebaseAdmin.auth().getUser(uid)
+		.then(function(user){
+			next();
+		})
+		.catch(function(error){
+			console.log(error);
+			response.redirect('/');
+		});
+}
 
 const app = express();
 app.use(logger('dev'));
@@ -20,6 +39,10 @@ app.get('/login', function(request, response) {
 
 app.get('/create', function(request, response) {
 	response.render('create.ejs');
+});
+
+app.get('/post', isAuthenticated, function(request, response) {
+	response.render('post.ejs');
 });
 
 const port = process.env.PORT || 8000;
